6294697b5d68670c9ae51b3ccac9c111c653dc2f
John John <44646589+SoloGuardiaN@users.noreply.github.com>
2018-11-05 08:41:04 -0600

    Setup floors
    Setup floors.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
-floor3 = []
-floor2 = []
-floor1 = []
+floor3 = ['sword', 'stairs down', 'nothing', ‘boss monster’, ‘prize’]
+floor2 = ['magic stones', 'stairs up', 'monster', ’stairs down’, ’nothing’]
+floor1 = ['nothing', 'sword', 'monster', ’stairs up’, ’sword’]
 inventory = [0,0,0]
 x = input("What do you do next?")
 #if x == "help"
