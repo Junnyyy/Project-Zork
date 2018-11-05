2e1f5f63ea42d03f6ca29736b012d4a09b18bf56
SirDerpken <44777181+SirDerpken@users.noreply.github.com>
2018-11-05 08:41:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ floor3 = ['sword', 'stairs down', 'nothing', ‘boss monster’, ‘prize’]
 floor2 = ['magic stones', 'stairs up', 'monster', ’stairs down’, ’nothing’]
 floor1 = ['nothing', 'sword', 'monster', ’stairs up’, ’sword’]
 inventory = [0,0,0]
+user_room = 0
+user_floor = 0
 x = input("What do you do next?")
 #if x == "help"
 # print("left, right, up, down, grab, fight, help")
