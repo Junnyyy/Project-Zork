e2601fadb8286647f8e1d436162d4c11690faf5c
John John <44646589+SoloGuardiaN@users.noreply.github.com>
2018-11-05 09:14:46 -0600

    Added comments

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 
+# Floor 1-3
 floor3 = ['sword', 'stairs down', 'nothing', 'boss monster', 'prize']
 floor2 = ['magic stones', 'stairs up', 'monster', 'stairs down', 'nothing']
 floor1 = ['nothing', 'sword', 'monster', 'stairs up', 'sword']
+# inventory, user room, and user floor variables 
 inventory = [0,0,0]
 user_room = 0
 user_floor = 1
 game_over = 0
 print("Welcome unfortunate victim, this is a test of your skills in combat and how fast you can think on your feet.  Currently you are locked in a warehouse there are 3 normal 'guests' and one very very special 'guest'. Can you defeat the 'guests' and retrieve the key to escape?  Which room on this floor would you like to go to? Or maybe you'd like to go to a different floor? Type 'help' for the commands.\n")
+# if statements for game function
 while game_over == 0:
   x = input("What do you do?")
   if x == 'help':
