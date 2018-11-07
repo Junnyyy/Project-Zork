f4456100ae2007b6840032fb12e6c3cdd8e0569d
SirDerpken <44777181+SirDerpken@users.noreply.github.com>
2018-11-07 08:25:27 -0600

    Nothing room

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ game_over = 0
 print("Welcome unfortunate victim, this is a test of your skills in combat and how fast you can think on your feet.  Currently you are locked in a warehouse there are 3 normal 'guests' and one very very special 'guest'. Can you defeat the 'guests' and retrieve the key to escape?  Which room on this floor would you like to go to? Or maybe you'd like to go to a different floor? Type 'help' for the commands.\n")
 # if statements for game function
 while game_over == 0:
+  if user_floor[user_room] == 'nothing':
+    print("This room has nothing in it")
   x = input("What do you do?")
   if x == 'help':
     print("left, right, up, down, grab, fight, help, end")
